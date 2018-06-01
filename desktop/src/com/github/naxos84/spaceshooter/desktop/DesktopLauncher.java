79e3e76bff80cb1c7751a1a2e1def94e17c484ee
MatthiasKolley <matthias.kolley@googlemail.com>
2018-06-01 23:40:49 +0200

    Added a lot of assets. created moveable ship.

--- desktop/src/com/github/naxos84/spaceshooter/desktop/DesktopLauncher.java
+++ desktop/src/com/github/naxos84/spaceshooter/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.naxos84.spaceshooter.SpaceShooter;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SpaceShooter(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 600;
+        config.width = 800;
+
+        new LwjglApplication(new SpaceShooter(), config);
+    }
 }
