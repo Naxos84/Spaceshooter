90c0643e8b9ea3152de38fa68c882805fa0fef19
Matthias Kolley <mko@clausohm.de>
2018-06-05 15:59:27 +0200

    Added basic ui for mainMenuScreen

--- core/src/com/github/naxos84/spaceshooter/screen/MainMenuScreen.java
+++ core/src/com/github/naxos84/spaceshooter/screen/MainMenuScreen.java
@@ -5,6 +5,14 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.github.naxos84.spaceshooter.SpaceShooter;
 
 import java.util.Locale;
@@ -12,21 +20,55 @@ import java.util.Locale;
 public class MainMenuScreen implements Screen {
 
     private final SpaceShooter game;
-    private OrthographicCamera camera;
     private final boolean debugMode;
+
+    private final Stage stage;
+
+    private TextButton startGame;
+    private TextButton exitGame;
 
     public MainMenuScreen(final SpaceShooter game, final boolean debugMode) {
         this.game = game;
         this.debugMode = debugMode;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 600);
-
+        stage = new Stage(new ScreenViewport());
     }
 
     @Override
     public void show() {
 
+        //Gdx.app.getPreferences("MY_PREFS").putString("LAST_START", "now").flush();
+
+
+        Gdx.input.setInputProcessor(stage);
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+
+        stage.addActor(table);
+
+        final Skin skin = new Skin(Gdx.files.internal("skin/star-soldier-ui.json"));
+
+        startGame = new TextButton(game.bundle.get("KEY_START_GAME"), skin);
+        startGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game, debugMode));
+                dispose();
+            }
+        });
+
+        exitGame = new TextButton(game.bundle.get("KEY_EXIT_GAME"), skin);
+        exitGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(startGame).fillX().uniformX();
+        table.row().pad(10, 0, 10, 0);
+        table.add(exitGame).fillX().uniformX();
     }
 
     @Override
@@ -34,31 +76,28 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.G)) {
+            changeLocale(Locale.GERMANY);
 
-        game.batch.begin();
-        game.font.draw(game.batch, game.bundle.get("KEY_WELCOME"), 100, 150);
-        game.font.draw(game.batch, game.bundle.get("KEY_CLICK_TO_BEGIN"), 100, 100);
-        game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game, debugMode));
-            dispose();
-        }
-
-        if(Gdx.input.isKeyJustPressed(Input.Keys.G)) {
-            game.setLocale(Locale.GERMANY);
         } else if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
-            game.setLocale(Locale.getDefault());
+            changeLocale(Locale.getDefault());
         } else if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
-            game.setLocale(Locale.UK);
+            changeLocale(Locale.UK);
         }
+
+        stage.act(Math.min(delta, 1 / 30f));
+        stage.draw();
+    }
+
+    private void changeLocale(final Locale locale) {
+        game.setLocale(locale);
+        startGame.setText(game.bundle.get("KEY_START_GAME"));
+        exitGame.setText(game.bundle.get("KEY_EXIT_GAME"));
     }
 
     @Override
     public void resize(final int width, final int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -78,6 +117,6 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
